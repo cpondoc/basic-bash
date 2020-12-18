e142057922a87bf451fbeeb5829bfda5e789bee1
Chris Pondoc <chris.pondoc@comcast.net>
2020-12-18 18:03:55 -0500

    add quotes

--- my.sh
+++ my.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # This is a basic bash script.
 ls
+
+# Single quotes - everything is printed literally
+echo '$what is up (world)!'
+
+# Double quotes - not as literal
+echo "$what is up (world)!"
